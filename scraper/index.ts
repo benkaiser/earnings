18d4794bd1ed765acc052ccd5c345bd10d7fcd08
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2020-07-19 19:03:34 -0700

    Tie prices to earnings dates

--- scraper/index.ts
+++ scraper/index.ts
@@ -11,6 +11,53 @@ interface IEstimate {
   reported: number;
 }
 
+interface IEstimateWithInfo extends IEstimate {
+  pre: {
+    oneDay: ITickerHistory;
+    twoDay: ITickerHistory;
+    threeDay: ITickerHistory;
+    oneWeek: ITickerHistory;
+    twoWeek: ITickerHistory;
+  };
+  post: {
+    oneDay: ITickerHistory;
+    twoDay: ITickerHistory;
+    threeDay: ITickerHistory;
+    oneWeek: ITickerHistory;
+    twoWeek: ITickerHistory;
+  };
+}
+
+interface IQuote {
+  close: number[],
+  high: number[],
+  low: number[],
+  open: number[],
+  volume: number[]
+};
+
+interface IPriceJson {
+  chart: {
+    result: Array<{
+      indicators: {
+        quote: Array<IQuote>
+      },
+      timestamp: Array<number>;
+    }>;
+  };
+}
+
+interface ITickerHistory {
+  timestamp: number;
+  date: string;
+  index: number;
+  close: number;
+  high: number;
+  open: number;
+  low: number;
+  volume: number;
+}
+
 console.log('- Started scrape');
 
 class CompanyScraper {
@@ -22,15 +69,14 @@ class CompanyScraper {
 
   public process(): Promise<void> {
     return Promise.all([
-      got(`https://query1.finance.yahoo.com/v8/finance/chart/${this.ticker}?interval=1d&range=10y`),
+      got(`https://query1.finance.yahoo.com/v8/finance/chart/${this.ticker}?interval=1d&range=50y`),
       got(`https://finance.yahoo.com/calendar/earnings?from=2020-07-12&to=2020-07-18&day=2020-07-13&symbol=${this.ticker}`)
-    ]).then((responses: Response[]) => {
-      // @ts-ignore
+    ]).then((responses: Response<any>[]) => {
       const [ priceResponse, earningsResponse ] = responses;
-      // @ts-ignore
+      const priceJson = this.processTickerPrices(priceResponse.body);
       const earnignsJson = this.processEarningsPage(earningsResponse.body);
-      console.log(earnignsJson);
-      // console.log(priceResponse.statusCode)
+      const earningsDecorated = this.correlateEarningsWithStockprice(priceJson, earnignsJson);
+      console.log(earningsDecorated.slice(0, 10));
     });
   }
 
@@ -50,6 +96,65 @@ class CompanyScraper {
     return combined;
   }
 
+  private processTickerPrices(pricesRaw: string): ITickerHistory[] {
+    const priceJson: IPriceJson = JSON.parse(pricesRaw);
+    const result = priceJson.chart.result[0];
+    return result.timestamp.map((timestamp, index) => {
+      return {
+        timestamp: timestamp,
+        date: moment(timestamp * 1000).format('YYYY-MM-DD'),
+        index: index,
+        close: result.indicators.quote[0].close[index],
+        high: result.indicators.quote[0].high[index],
+        low: result.indicators.quote[0].low[index],
+        open: result.indicators.quote[0].open[index],
+        volume: result.indicators.quote[0].volume[index],
+      };
+    });
+  }
+
+  private correlateEarningsWithStockprice(tickerPrices: ITickerHistory[], earningsJson: IEstimate[]): IEstimateWithInfo[] {
+    return earningsJson.map((earning) => {
+      return {
+        ...earning,
+        pre: {
+          oneDay: this.getPriceFor(tickerPrices, earning.date),
+          twoDay: this.getPriceFor(tickerPrices, earning.date, -1),
+          threeDay: this.getPriceFor(tickerPrices, earning.date, -2),
+          oneWeek: this.getPriceFor(tickerPrices, earning.date, -5),
+          twoWeek: this.getPriceFor(tickerPrices, earning.date, -10),
+        },
+        post: {
+          oneDay: this.getPriceFor(tickerPrices, earning.date, 1),
+          twoDay: this.getPriceFor(tickerPrices, earning.date, 2),
+          threeDay: this.getPriceFor(tickerPrices, earning.date, 3),
+          oneWeek: this.getPriceFor(tickerPrices, earning.date, 6),
+          twoWeek: this.getPriceFor(tickerPrices, earning.date, 11),
+        },
+      };
+    });
+  }
+
+  private getPriceFor(tickerPrices: ITickerHistory[], date: string, daysAhead: number = 0): ITickerHistory {
+    const tickerNotFound: ITickerHistory = {
+      timestamp: NaN,
+      date: '',
+      index: NaN,
+      volume: NaN,
+      open: NaN,
+      close: NaN,
+      high: NaN,
+      low: NaN
+    };
+    for (let i = 0; i < tickerPrices.length; i++) {
+      if (tickerPrices[i].date === date) {
+        return tickerPrices[i + daysAhead] || tickerNotFound;
+      }
+    }
+    console.log(`Unable to find ticker for date ${date} on symbol ${this.ticker}`);
+    return tickerNotFound;
+  }
+
   private processDate(date: string): string {
     return moment(date.split(',').slice(0,2).join(',')).format('YYYY-MM-DD');
   }
@@ -61,7 +166,7 @@ const COMPANY_LIST: string[] = [
   // 'AMZN',
 ];
 COMPANY_LIST.forEach(ticker => {
-  new CompanyScraper(ticker).process(); 
+  new CompanyScraper(ticker).process();
 });
 
 
