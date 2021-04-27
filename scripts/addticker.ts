6c08440ef02811adfa970ecd9c778931819b750c
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-04-27 22:35:33 +1000

    Drop useful info

--- scripts/addticker.ts
+++ scripts/addticker.ts
@@ -1,3 +1,13 @@
-console.log(process.env.GITHUB_CONTEXT);
-console.log(process.env);
-console.log(JSON.parse(process.env.GITHUB_CONTEXT!));
+export interface IGithubIssue {
+  body: string;
+  id: number;
+  title: string;
+  labels: string[];
+}
+
+const GITHUB_ISSUE: IGithubIssue = JSON.parse(process.env.GITHUB_CONTEXT!) as IGithubIssue;
+
+console.log(GITHUB_ISSUE.body);
+console.log(GITHUB_ISSUE.title);
+console.log(GITHUB_ISSUE.labels);
+console.log(GITHUB_ISSUE.id);
