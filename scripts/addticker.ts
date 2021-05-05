c84856549fba92ab313ba9b6f3844020cb72b4bc
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2021-05-05 20:38:08 +1000

    Switch to repo dispatch trigger

--- scripts/addticker.ts
+++ scripts/addticker.ts
@@ -1,37 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-export interface ILabel {
-  name: string;
+export interface IActionInput {
+  ticker: string;
+  type: string;
 }
 
-export interface IGithubIssue {
-  body: string;
-  id: number;
-  title: string;
-  labels: ILabel[];
-}
+console.log(process.env.GITHUB_CONTEXT);
+const ACTION_INPUTS: IActionInput = JSON.parse(process.env.GITHUB_CONTEXT!) as IActionInput;
+console.log(ACTION_INPUTS);
 
-const GITHUB_ISSUE: IGithubIssue = JSON.parse(process.env.GITHUB_CONTEXT!) as IGithubIssue;
-
-if (GITHUB_ISSUE.labels.filter(label => label.name === 'addticker')) {
-  console.log('Found addticker label');
-  try {
-    const ticker: string = GITHUB_ISSUE.title.match(/:\s+(.+)/)![1].trim().toUpperCase();
-    const announceType: string = GITHUB_ISSUE.body.match(/:\s+(.+)/)![1].trim().toLocaleLowerCase();
-    console.log('Ticker: ' + ticker);
-    console.log('Announces: ' + announceType);
-    if (announceType !== 'pre' && announceType !== 'post') {
-      console.error('Invalid annoucement type, must be pre or post');
-      process.exit(1);
-    }
-    const companyFile = path.join(__dirname, '../shared/CompanyList.json');
-    const Companies = JSON.parse(fs.readFileSync(companyFile));
-    Companies[ticker] = { type: announceType };
-    fs.writeFileSync(companyFile, JSON.stringify(Companies, null, 2));
-    console.log('Attempting scrape');
-    require('./scraper');
-  } catch (error) {
-    console.error(error);
+try {
+  const ticker: string = ACTION_INPUTS.ticker.toUpperCase();
+  const announceType: string = ACTION_INPUTS.type.toUpperCase();
+  console.log('Ticker: ' + ticker);
+  console.log('Announces: ' + announceType);
+  if (announceType !== 'pre' && announceType !== 'post') {
+    console.error('Invalid annoucement type, must be pre or post');
+    process.exit(1);
   }
+  const companyFile = path.join(__dirname, '../shared/CompanyList.json');
+  const Companies = JSON.parse(fs.readFileSync(companyFile));
+  Companies[ticker] = { type: announceType };
+  fs.writeFileSync(companyFile, JSON.stringify(Companies, null, 2));
+  console.log('Attempting scrape');
+  require('./scraper');
+} catch (error) {
+  console.error(error);
 }
